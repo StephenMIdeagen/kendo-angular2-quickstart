ac219de423e961920494ce5fb1788ae451dc6c84
Petyo Ivanov <underlog@gmail.com>
2016-09-14 16:45:13 +0300

    feat: add Kendo UI buttons package

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    styleUrls: [ '../node_modules/@progress/kendo-angular-buttons/dist/npm/css/main.css' ], // load the button theme
+    template: `
+    <h1>My First Kendo UI Angular 2 App</h1>
+    <button kendoButton (click)="onButtonClick()" [primary]=true>My Kendo UI Button</button>
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+   onButtonClick() {
+        alert('Hello from Kendo UI!');
+    }
+}
